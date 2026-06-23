68620f803cc2501e0f8e1b43153a02528d27cbc2
卄ㄚ卩乇尺几ㄖᐯ卂✨ <mhmmdeymngrbz@gmail.com>
2026-06-23 19:31:59 +0300

    Refactor CapsuleSettings to use #define directives

--- src/capsule++.cpp
+++ src/capsule++.cpp
@@ -4,28 +4,25 @@
 #include <FS.h>
 #include <SD.h>
 
-namespace CapsuleSettings
-{
-    const int pinDHT = D0;
-    const int pinMQAnalog = A0;
-    const int pinMQDigital = D14;
-    const int pinAcidAnalog = A1;
-    const int pinHeater = D3;
+#define pinDHT D0
+#define pinMQAnalog A0
+#define pinMQDigital D14
+#define pinAcidAnalog A1
+#define pinHeater D3
 
-    const int pinMotor1 = D1;
-    const int pinMotor2 = D2;
+#define pinMotor1 D1
+#define pinMotor2 D2
 
-    const int pwmChannel1 = 0;
-    const int pwmChannel2 = 1;
-    const int pwmFrequency = 5000;
-    const int pwmResolution = 8;
+#define pwmChannel1 0
+#define pwmChannel2 1
+#define pwmFrequency 5000
+#define pwmResolution 8
 
-    const float THRESHOLD_CRITICAL_TEMP = 45.0;
-    const int THRESHOLD_ACID_LEAK = 500;
-    const int THRESHOLD_POLLUTED_AIR = 600;
+#define THRESHOLD_CRITICAL_TEMP 45.0
+#define THRESHOLD_ACID_LEAK 500
+#define THRESHOLD_POLLUTED_AIR 600
 
-    const int SD_CS_PIN = D10;
-}
+#define SD_CS_PIN D10
 
 struct CapsuleData
 {
@@ -48,17 +45,19 @@ private:
     File logFile;
     bool sdAvailable = false;
     unsigned long lastLogTime = 0;
-    const unsigned long logInterval = 5000;
+    const unsigned long logInterval = 1500;
+    unsigned long lastDisplayTime = 0;
+    const unsigned long displayInterval = 1500;
 
     void showBootAnimation()
     {
         Serial.println("=========================================");
-        Serial.println("      Initializing Core Systems...       ");
+        Serial.println("      Initializing Core Systems...        ");
         Serial.println("=========================================");
         delay(300);
-        Serial.println("   [  ] Loading Kernel...                ");
+        Serial.println("   [  ] Loading Kernel...                 ");
         delay(300);
-        Serial.println("   [==] Mounting SD Card...              ");
+        Serial.println("   [==] Mounting SD Card...               ");
         delay(300);
         Serial.println("   [====] Calibrating Environment...     ");
         delay(400);
@@ -90,7 +89,7 @@ private:
     }
 
 public:
-    PlanetCapsule() : dht11(CapsuleSettings::pinDHT), lcd(0x27, 20, 4)
+    PlanetCapsule() : dht11(pinDHT), lcd(0x27, 20, 4)
     {
     }
 
@@ -102,14 +101,14 @@ public:
         lcd.init();
         lcd.backlight();
 
-        pinMode(CapsuleSettings::pinMQDigital, INPUT);
-        pinMode(CapsuleSettings::pinHeater, OUTPUT);
-        digitalWrite(CapsuleSettings::pinHeater, LOW);
+        pinMode(pinMQDigital, INPUT);
+        pinMode(pinHeater, OUTPUT);
+        digitalWrite(pinHeater, LOW);
         
-        ledcSetup(CapsuleSettings::pwmChannel1, CapsuleSettings::pwmFrequency, CapsuleSettings::pwmResolution);
-        ledcSetup(CapsuleSettings::pwmChannel2, CapsuleSettings::pwmFrequency, CapsuleSettings::pwmResolution);
-        ledcAttachPin(CapsuleSettings::pinMotor1, CapsuleSettings::pwmChannel1);
-        ledcAttachPin(CapsuleSettings::pinMotor2, CapsuleSettings::pwmChannel2);
+        ledcSetup(pwmChannel1, pwmFrequency, pwmResolution);
+        ledcSetup(pwmChannel2, pwmFrequency, pwmResolution);
+        ledcAttachPin(pinMotor1, pwmChannel1);
+        ledcAttachPin(pinMotor2, pwmChannel2);
 
         showBootAnimation();
 
@@ -118,7 +117,7 @@ public:
         lcd.setCursor(0, 1);
         lcd.print("Checking SD Card...");
         
-        if (!SD.begin(CapsuleSettings::SD_CS_PIN))
+        if (!SD.begin(SD_CS_PIN))
         {
             Serial.println("SD ERROR!");
             lcd.setCursor(0, 2);
@@ -157,10 +156,10 @@ public:
             data.humidity = (float)humidByte;
         }
 
-        data.gasLevel = analogRead(CapsuleSettings::pinMQAnalog);
-        data.gasDanger = digitalRead(CapsuleSettings::pinMQDigital) == HIGH || (data.gasLevel > CapsuleSettings::THRESHOLD_POLLUTED_AIR);
-        data.acidLevel = analogRead(CapsuleSettings::pinAcidAnalog);
-        data.acidLeakDetected = data.acidLevel > CapsuleSettings::THRESHOLD_ACID_LEAK;
+        data.gasLevel = analogRead(pinMQAnalog);
+        data.gasDanger = digitalRead(pinMQDigital) == HIGH || (data.gasLevel > THRESHOLD_POLLUTED_AIR);
+        data.acidLevel = analogRead(pinAcidAnalog);
+        data.acidLeakDetected = data.acidLevel > THRESHOLD_ACID_LEAK;
 
         if (data.acidLeakDetected)
         {
@@ -170,7 +169,7 @@ public:
         {
             data.systemStatus = "AIR FILTER MODE";
         }
-        else if (data.temperature > CapsuleSettings::THRESHOLD_CRITICAL_TEMP)
+        else if (data.temperature > THRESHOLD_CRITICAL_TEMP)
         {
             data.systemStatus = "OVERHEATING!";
         }
@@ -182,14 +181,14 @@ public:
 
     void distillationManager()
     {
-        if (data.temperature < CapsuleSettings::THRESHOLD_CRITICAL_TEMP && data.acidLevel > 100)
+        if (data.temperature < THRESHOLD_CRITICAL_TEMP && data.acidLevel > 100)
         {
-            digitalWrite(CapsuleSettings::pinHeater, HIGH);
+            digitalWrite(pinHeater, HIGH);
             data.heaterStatus = true;
         }
         else
         {
-            digitalWrite(CapsuleSettings::pinHeater, LOW);
+            digitalWrite(pinHeater, LOW);
             data.heaterStatus = false;
         }
     }
@@ -203,35 +202,40 @@ public:
             fanSpeed = map(constrain(data.temperature, 25.0, 50.0), 25, 50, 50, 255);
         }
 
-        if (data.acidLeakDetected || data.temperature > CapsuleSettings::THRESHOLD_CRITICAL_TEMP)
+        if (data.acidLeakDetected || data.temperature > THRESHOLD_CRITICAL_TEMP)
         {
             fanSpeed = 255;
         }
 
-        ledcWrite(CapsuleSettings::pwmChannel1, fanSpeed);
-        ledcWrite(CapsuleSettings::pwmChannel2, fanSpeed);
+        ledcWrite(pwmChannel1, fanSpeed);
+        ledcWrite(pwmChannel2, fanSpeed);
     }
 
     void updateDisplay()
     {
-        lcd.setCursor(0, 0);
-        lcd.print("CP++: " + data.systemStatus.substring(0, 14)); 
-        
-        lcd.setCursor(0, 1);
-        lcd.print("T:" + String(data.temperature, 1) + "C  H:%" + String(data.humidity, 0) + "   ");
-
-        lcd.setCursor(0, 2);
-        lcd.print("Gas:" + String(data.gasLevel) + (data.heaterStatus ? " HT:ON " : " HT:OFF"));
-
-        lcd.setCursor(0, 3);
-        
-        if (data.acidLeakDetected)
+        if (millis() - lastDisplayTime >= displayInterval)
         {
-            lcd.print("ACID LEAK DETECTED!! ");
-        }
-        else
-        {
-            lcd.print("Acid/Liq: " + String(data.acidLevel) + " [OK]  ");
+            lastDisplayTime = millis();
+
+            lcd.setCursor(0, 0);
+            lcd.print("CP++: " + data.systemStatus.substring(0, 14)); 
+            
+            lcd.setCursor(0, 1);
+            lcd.print("T:" + String(data.temperature, 1) + "C  H:%" + String(data.humidity, 0) + "   ");
+
+            lcd.setCursor(0, 2);
+            lcd.print("Gas:" + String(data.gasLevel) + (data.heaterStatus ? " HT:ON " : " HT:OFF"));
+
+            lcd.setCursor(0, 3);
+            
+            if (data.acidLeakDetected)
+            {
+                lcd.print("ACID LEAK DETECTED!! ");
+            }
+            else
+            {
+                lcd.print("Acid/Liq: " + String(data.acidLevel) + " [OK]  ");
+            }
         }
     }
 
@@ -276,5 +280,4 @@ void loop()
     capsule.coolingManager();
     capsule.updateDisplay();
     capsule.logToSD();
-    delay(200);
 }
